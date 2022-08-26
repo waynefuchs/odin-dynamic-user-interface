3bb584506ae50ff76b2639bbc04dc78999ba583a
Wayne Fuchs <wayne.fuchs@icloud.com>
2022-08-26 12:37:40 -0500

    Unknown

--- src/index.js
+++ src/index.js
@@ -1,3 +1,20 @@
-import './style.css';
+import "./style.css";
 
-console.log('MOO');
+function clearClass(classText) {
+  const dropdowns = Array.from(document.querySelectorAll(`.${classText}`));
+  dropdowns.classList.remove(classText);
+}
+
+function dropdown(element) {
+  const dropdowns = Array.from(element.querySelectorAll(".drop-menu"));
+  dropdowns.forEach((li) => {
+    li.classList.add("processed");
+    li.addEventListener("click", (event) => {
+      clearClass("clicked");
+      event.target.classList.add("clicked");
+    });
+  });
+}
+
+dropdown(document.querySelector("nav>ul"));
+console.log("MOO");
