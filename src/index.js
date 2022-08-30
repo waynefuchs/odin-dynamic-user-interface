3f1761fdd280ec84585e6d630eb4d193c1aa685f
Wayne Fuchs <wayne.fuchs@icloud.com>
2022-08-30 13:59:45 -0500

    Fix typo causing dropdown to not work and refactor

--- src/index.js
+++ src/index.js
@@ -1,23 +1,33 @@
 import "./style.css";
 
-function clearClass(classText) {
-  console.log('clearing ' + classText);
-  const allClicked = Array.from(document.querySelectorAll(`.${classText}`));
-  allClicked?.classList?.remove(classText);
+function clearClass(classText, except=null) {
+  const allClicked = Array.from(document.querySelectorAll(`.${classText}`)).filter(element => element !== except);
+  allClicked?.forEach(clicked => clicked.classList.remove(classText));
+}
+
+function addEventMouseEnter(li) {
+  li.addEventListener('mouseenter', (event) => {
+    clearClass('clicked', event.target);
+    event.stopPropagation();
+  });
+}
+
+function addEventMouseClick(li) {
+  li.addEventListener("click", (event) => {
+    const isClicked = event.target.classList.contains('clicked');
+    clearClass('clicked');
+    if (!isClicked)
+      event.target.classList.add('clicked');
+  });
 }
 
 function dropdown(element) {
   const dropdowns = Array.from(element.querySelectorAll(".drop-menu"));
   dropdowns.forEach((li) => {
     li.classList.add("processed");
-    li.addEventListener("click", (event) => {
-      clearClass("clicked");
-      event.target.classList.add("clicked");
-    });
+    addEventMouseEnter(li);
+    addEventMouseClick(li);
   });
 }
 
 dropdown(document.querySelector("nav>ul"));
-
-
-console.log("MOO");
