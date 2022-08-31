3ea392d496b872bf8145c78d00483f14565d3272
Wayne Fuchs <wayne.fuchs@icloud.com>
2022-08-30 23:17:57 -0500

    Refactor into odin-npm/dropdown

--- src/index.js
+++ src/index.js
@@ -1,33 +1,4 @@
 import "./style.css";
+import {attach} from 'dropdown';
 
-function clearClass(classText, except=null) {
-  const allClicked = Array.from(document.querySelectorAll(`.${classText}`)).filter(element => element !== except);
-  allClicked?.forEach(clicked => clicked.classList.remove(classText));
-}
-
-function addEventMouseEnter(li) {
-  li.addEventListener('mouseenter', (event) => {
-    clearClass('clicked', event.target);
-    event.stopPropagation();
-  });
-}
-
-function addEventMouseClick(li) {
-  li.addEventListener("click", (event) => {
-    const isClicked = event.target.classList.contains('clicked');
-    clearClass('clicked');
-    if (!isClicked)
-      event.target.classList.add('clicked');
-  });
-}
-
-function dropdown(element) {
-  const dropdowns = Array.from(element.querySelectorAll(".drop-menu"));
-  dropdowns.forEach((li) => {
-    li.classList.add("processed");
-    addEventMouseEnter(li);
-    addEventMouseClick(li);
-  });
-}
-
-dropdown(document.querySelector("nav>ul"));
+attach('.drop-menu');
