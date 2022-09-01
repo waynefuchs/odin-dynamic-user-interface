a661fc7f12357a93056bc87d3af86660d797ca39
Wayne Fuchs <wayne.fuchs@icloud.com>
2022-09-01 01:31:49 -0500

    Big push to finish up

--- src/index.js
+++ src/index.js
@@ -1,4 +1,11 @@
+import {dropdownAttach} from 'dropdown';
+import {carouselAttach} from 'carousel';
 import "./style.css";
-import {attach} from 'dropdown';
 
-attach();
+import ci1 from "./img/bailey-zindel-NRQV-hBF10M-unsplash.jpg";
+import ci2 from "./img/john-fowler-aaIN3y2zcMQ-unsplash.jpg";
+import ci3 from "./img/jasper-boer-LJD6U920zVo-unsplash.jpg";
+import ci4 from "./img/mark-harpur-K2s_YE031CA-unsplash.jpg";
+
+dropdownAttach();
+carouselAttach([ci1, ci2, ci3, ci4]);
--- webpack.config.js
+++ webpack.config.js
@@ -1,36 +1,46 @@
-const path = require('path');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
-  mode: 'development',
+  mode: "development",
   // mode: 'production',
   entry: {
-    index: './src/index.js',
+    index: "./src/index.js",
   },
 
   module: {
     rules: [
       {
         test: /\.css$/i,
-        use: ['style-loader', 'css-loader'],
+        use: ["style-loader", "css-loader"],
+      },
+      {
+        test: /\.(png|jpg|gif)$/,
+        use: [
+          {
+            loader: "file-loader",
+            options: {
+              name: 'img/[name].[ext]'
+            },
+          },
+        ],
       },
     ],
   },
 
-  devtool: 'inline-source-map',
+  devtool: "inline-source-map",
   plugins: [
     new HtmlWebpackPlugin({
       hash: true,
-      title: 'Dynamic User Interface',
-      myPageHeader: 'Hello World',
-      template: './src/index.html',
-      filename: './index.html'
-    })
-
+      title: "Dynamic User Interface",
+      myPageHeader: "Hello World",
+      template: "./src/index.html",
+      filename: "./index.html",
+    }),
   ],
   output: {
-    filename: '[name].bundle.js',
-    path: path.resolve(__dirname, 'dist'),
+    filename: "[name].bundle.js",
+    path: path.resolve(__dirname, "dist"),
     clean: true,
   },
 };
